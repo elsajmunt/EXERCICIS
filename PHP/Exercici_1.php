bcc2d85af78776793aabf67d4662f3e3ee6c23fe
elsajmunt <71447570+elsajmunt@users.noreply.github.com>
2024-04-09 13:42:03 +0200

    Add files via upload

---
+++ PHP/Exercici_1.php
@@ -0,0 +1,13 @@
+<HTML>
+<HEAD>
+<TITLE>Mi primer programa en PHP</TITLE>
+<h2>Apunts php part 1.pdf</h2>
+<h1>EJERCICIO 1</h1>
+</HEAD>
+<BODY>
+    <?PHP
+        print("Hola mundo"); // print: muestra una cadena
+    // print(“<P>Hola mundo</P>”); <<< amb HTML a dins No funciona!!???
+    ?>
+</BODY>
+</HTML>
