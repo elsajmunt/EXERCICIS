02112d3043d45d2e1bbe12ac4ee62cb150c049d8
elsajmunt <71447570+elsajmunt@users.noreply.github.com>
2024-05-22 14:09:16 +0200

    Add files via upload

---
+++ JAVASCRIPT/Examen_JS/vectorArray_Grups.js
@@ -0,0 +1,32 @@
+[
+{
+‘nom’: ‘Tu no ets de Vilanova si no …’,
+‘descripcio’: ‘...’,
+‘imatge’: ‘http://xxxxxxxx’
+‘tags’:[ ‘social’]
+},
+{
+‘nom’: ‘Antics alumnes Escola XXXX’,
+‘descripcio’:: ‘...’,
+‘imatge’: ‘http://xxxxxxxx
+‘tags’: [‘social’,’records’’]
+},
+{
+‘nom’: ‘Vilanova imatges d’ahir i avui’,
+‘descripcio’: ‘...’,
+‘imatge’: ‘http://xxxxxxxx
+‘tags’: [‘records’]
+},
+{
+‘nom’: ‘Anem a caminar’,
+‘description’: ‘...’,
+‘imatge’: ‘http://xxxxxxxx
+‘tags’: [‘esport’, ‘salut’]
+},
+{
+‘nom’: ‘Club Esportiu Ribes’,
+‘descripcio’:: ‘...’,
+‘imatge’: ‘http://xxxxxxxx
+‘tags’: [‘esport’, ‘aventura’]
+}
+]
