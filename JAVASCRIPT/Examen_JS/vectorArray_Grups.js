e9bff187a926a6c1f13745dd4bfbb6dc4de7a99c
elsajmunt <71447570+elsajmunt@users.noreply.github.com>
2024-05-22 23:29:33 +0200

    Add files via upload

--- JAVASCRIPT/Examen_JS/vectorArray_Grups.js
+++ JAVASCRIPT/Examen_JS/vectorArray_Grups.js
@@ -1,32 +1,36 @@
 [
-{
-‘nom’: ‘Tu no ets de Vilanova si no …’,
-‘descripcio’: ‘...’,
-‘imatge’: ‘http://xxxxxxxx’
-‘tags’:[ ‘social’]
-},
-{
-‘nom’: ‘Antics alumnes Escola XXXX’,
-‘descripcio’:: ‘...’,
-‘imatge’: ‘http://xxxxxxxx
-‘tags’: [‘social’,’records’’]
-},
-{
-‘nom’: ‘Vilanova imatges d’ahir i avui’,
-‘descripcio’: ‘...’,
-‘imatge’: ‘http://xxxxxxxx
-‘tags’: [‘records’]
-},
-{
-‘nom’: ‘Anem a caminar’,
-‘description’: ‘...’,
-‘imatge’: ‘http://xxxxxxxx
-‘tags’: [‘esport’, ‘salut’]
-},
-{
-‘nom’: ‘Club Esportiu Ribes’,
-‘descripcio’:: ‘...’,
-‘imatge’: ‘http://xxxxxxxx
-‘tags’: [‘esport’, ‘aventura’]
-}
+    {
+    nom: "Tu no ets de Vilanova si no …",
+    descripcio: " …",
+    imatge: "http/xxx",
+    tags:["social"],
+    },
+
+    {
+    nom: "Antics alumnes Escola XXXX",
+    descripcio: " …",
+    imatge: "http/xxx",
+    tags: ["social, records"],
+    },
+
+    {
+    nom: "Vilanova imatges d'ahir i avui",
+    descripcio: " …",
+    imatge: "http/xxx",
+    tags: ["records"],
+    },
+
+    {
+    nom: "Anem a caminar",
+    descripcio: " …",
+    imatge: "http/xxx",
+    tags:["esport, salut"],
+    },
+    
+    {
+    nom: "Club Esportiu Ribes",
+    descripcio: " …",
+    imatge: "http/xxx",
+    tags:["esport", "aventura"],
+    },     
 ]
