b733b0c36673a1562b294672fdb4bdd920963310
elsajmunt <71447570+elsajmunt@users.noreply.github.com>
2024-06-19 13:13:17 +0200

    Add files via upload

--- BASES DE DADES/xarxa_social.sql
+++ BASES DE DADES/xarxa_social.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-05-2024 a las 12:36:01
+-- Tiempo de generación: 19-06-2024 a las 13:10:38
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,12 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `enquesta`
+--
+
+CREATE TABLE `enquesta` (
+  `id` int(11) NOT NULL,
+  `titol` varchar(50) NOT NULL,
+  `opcio1` varchar(25) NOT NULL,
+  `opcio2` varchar(25) NOT NULL,
+  `idUsuari` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `enquesta`
+--
+
+INSERT INTO `enquesta` (`id`, `titol`, `opcio1`, `opcio2`, `idUsuari`) VALUES
+(1, 'menjar', '', '', 3),
+(2, 'comics', '', '', 3),
+(3, 'viatges', '', '', 10),
+(4, 'esports', '', '', 3),
+(5, 'Quina és la teva Formació?', 'Java Script', 'Html 5', 7),
+(6, 'Escull una película', 'Incendis', 'La vida es bella', 3),
+(7, 'Quin és el teu actor preferit?', 'Brat Pit', 'Antonio Banderas', 7),
+(8, 'Quin es el teu transport preferit?', 'Tren', 'Bicicleta', 4);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `missatges`
 --
 
 CREATE TABLE `missatges` (
   `id` tinyint(4) NOT NULL,
-  `id_usuari` tinyint(4) NOT NULL,
+  `id_usuari` int(11) NOT NULL,
   `Missatge` varchar(250) NOT NULL,
   `Data` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -39,15 +67,70 @@ CREATE TABLE `missatges` (
 --
 
 INSERT INTO `missatges` (`id`, `id_usuari`, `Missatge`, `Data`) VALUES
-(1, 0, 'Hola, proba envia missatge!', '0000-00-00'),
-(2, 0, 'Bon dia! Segon missatge', '0000-00-00'),
+(1, 1, 'Hola, proba envia missatge!', '0000-00-00'),
+(2, 1, 'Bon dia! Segon missatge', '0000-00-00'),
 (3, 3, 'Prova missatge: surt id usuari i data???', '0000-00-00'),
 (4, 4, 'Prova si surt data auto Números', '2024-05-31'),
 (5, 3, 'Usuari 3 envia un nou missatge', '2024-05-31'),
-(6, 3, 'Input tuype text', '2024-05-31'),
 (7, 3, 'Prova desde input type text', '2024-05-31'),
 (8, 3, 'Text Area Escriu aquí els teus comentaris...', '2024-05-31'),
-(9, 3, 'Comentari text area final :)\r\nJaja', '2024-05-31');
+(10, 3, 'Prova Missatge 12/6/ Elsa', '2024-06-12'),
+(12, 3, 'Un altre Missatge, des de formulari_missatge.php Elsa', '2024-06-13');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opcionsenquesta`
+--
+
+CREATE TABLE `opcionsenquesta` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `idEnquesta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `opcionsenquesta`
+--
+
+INSERT INTO `opcionsenquesta` (`id`, `nom`, `idEnquesta`) VALUES
+(1, 'Pizza', 1),
+(2, 'París', 3),
+(3, 'Verdura', 1),
+(4, 'Mortadelo', 2),
+(5, 'Filemón', 2),
+(6, 'Professor Bacteri', 2),
+(7, 'Calamars', 1),
+(8, 'Volei platja', 4),
+(9, 'Tofu', 1),
+(10, 'Futbol', 4),
+(11, 'Carn', 1),
+(12, 'Roma', 3),
+(13, 'Amsterdam', 3),
+(15, 'Barrufet rondinaire', 2),
+(16, 'pop a la gallega', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `resultatsenquesta`
+--
+
+CREATE TABLE `resultatsenquesta` (
+  `idUsuari` int(11) NOT NULL,
+  `idOpcioEnquesta` int(11) NOT NULL,
+  `data` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `resultatsenquesta`
+--
+
+INSERT INTO `resultatsenquesta` (`idUsuari`, `idOpcioEnquesta`, `data`) VALUES
+(1, 1, '2024-06-01'),
+(1, 2, '2024-06-04'),
+(3, 5, '0000-00-00'),
+(7, 1, '2024-06-04');
 
 -- --------------------------------------------------------
 
@@ -56,7 +139,7 @@ INSERT INTO `missatges` (`id`, `id_usuari`, `Missatge`, `Data`) VALUES
 --
 
 CREATE TABLE `usuaris` (
-  `id` tinyint(4) NOT NULL,
+  `id` int(11) NOT NULL,
   `Nom` varchar(50) NOT NULL,
   `Email` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL
@@ -67,25 +150,49 @@ CREATE TABLE `usuaris` (
 --
 
 INSERT INTO `usuaris` (`id`, `Nom`, `Email`, `Password`) VALUES
-(1, 'Lola', '0', '0'),
-(2, 'Carles', '0', '6789'),
-(3, 'Elsa', '0', '12345'),
+(1, 'Lola', 'lola@mail.com', 'lola'),
+(2, 'Carles', 'carles@mail.com', 'carles'),
+(3, 'Elsa', 'elsa@mail.com', 'elsa'),
 (4, 'Eva', 'eva@mail.com', 'eva'),
-(5, 'Ricard', 'ricard@hotmail.com', 'rikkki'),
-(6, 'Toni', 'toni@gmail.com', 'toni'),
+(5, 'Ricard', 'ricard@mail.com', 'ricard'),
+(6, 'Toni', 'toni@mail.com', 'toni'),
 (7, 'Anna', 'anna@mail.com', 'anna'),
 (8, 'Eric', 'eric@mail.com', 'eric'),
-(9, 'Mark', 'mark@mail.com', 'mark');
+(9, 'Mark', 'mark@mail.com', 'mark'),
+(10, 'Antonia', 'antonia@mail.com', 'antonia');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `enquesta`
+--
+ALTER TABLE `enquesta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idUsuari` (`idUsuari`);
+
+--
 -- Indices de la tabla `missatges`
 --
 ALTER TABLE `missatges`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuari` (`id_usuari`);
+
+--
+-- Indices de la tabla `opcionsenquesta`
+--
+ALTER TABLE `opcionsenquesta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idEnquesta` (`idEnquesta`);
+
+--
+-- Indices de la tabla `resultatsenquesta`
+--
+ALTER TABLE `resultatsenquesta`
+  ADD PRIMARY KEY (`idUsuari`,`idOpcioEnquesta`),
+  ADD KEY `idUsuari` (`idUsuari`,`idOpcioEnquesta`),
+  ADD KEY `idOpcioEnquesta` (`idOpcioEnquesta`);
 
 --
 -- Indices de la tabla `usuaris`
@@ -98,16 +205,57 @@ ALTER TABLE `usuaris`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `enquesta`
+--
+ALTER TABLE `enquesta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
+
+--
 -- AUTO_INCREMENT de la tabla `missatges`
 --
 ALTER TABLE `missatges`
-  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `opcionsenquesta`
+--
+ALTER TABLE `opcionsenquesta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `usuaris`
 --
 ALTER TABLE `usuaris`
-  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `enquesta`
+--
+ALTER TABLE `enquesta`
+  ADD CONSTRAINT `enquesta_ibfk_1` FOREIGN KEY (`idUsuari`) REFERENCES `usuaris` (`id`);
+
+--
+-- Filtros para la tabla `missatges`
+--
+ALTER TABLE `missatges`
+  ADD CONSTRAINT `missatges_ibfk_1` FOREIGN KEY (`id_usuari`) REFERENCES `usuaris` (`id`);
+
+--
+-- Filtros para la tabla `opcionsenquesta`
+--
+ALTER TABLE `opcionsenquesta`
+  ADD CONSTRAINT `opcionsenquesta_ibfk_1` FOREIGN KEY (`idEnquesta`) REFERENCES `enquesta` (`id`);
+
+--
+-- Filtros para la tabla `resultatsenquesta`
+--
+ALTER TABLE `resultatsenquesta`
+  ADD CONSTRAINT `resultatsenquesta_ibfk_1` FOREIGN KEY (`idUsuari`) REFERENCES `usuaris` (`id`),
+  ADD CONSTRAINT `resultatsenquesta_ibfk_2` FOREIGN KEY (`idOpcioEnquesta`) REFERENCES `opcionsenquesta` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
