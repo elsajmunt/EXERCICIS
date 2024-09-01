161a82f5dc0a0f0cec78b5b52ac04386dc2a9f0a
elsajmunt <71447570+elsajmunt@users.noreply.github.com>
2024-09-01 20:54:00 +0200

    Delete projectefinal_ceroresiduo directory

--- projectefinal_ceroresiduo/Config/Routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Routes {
-    const INDEX = 'http://localhost/PHP_OOP_agencies/View/login.php';
-    const HOME = 'http://localhost/PHP_OOP_agencies/View/home.php';
-    const NEW_PIS = 'http://localhost/PHP_OOP_agencies/View/create-pis.php';
-}
--- projectefinal_ceroresiduo/Service/Auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Auth {
-    public static function authenticate(string $password, string $hash): bool {
-        /* return password_verify($password, $hash); */
-        return $password === $hash; // Las contraseñas se guardan en texto plano de momento
-    }
-}
